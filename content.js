cfbbbb042e8a9117f9b422d670242f73fc34b2aa
Amardeep Mann <amar2slick@gmail.com>
2023-11-05 21:13:58 -0800

    Append a button to webpage body

---
+++ content.js
@@ -0,0 +1,11 @@
+// Create a button element
+var button = document.createElement("button");
+button.innerHTML = "Click me!";
+
+// Append the button to the body of the page
+document.body.appendChild(button);
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+  alert("Button clicked!");
+});
