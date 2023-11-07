ddee2736f009a67e02231c3b01bfccee117c7d18
Amardeep Mann <amar2slick@gmail.com>
2023-11-06 19:42:34 -0800

    Move button to top of body

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ var button = document.createElement("button");
 button.innerHTML = "Click me!";
 
 // Append the button to the body of the page
-document.body.appendChild(button);
+document.body.prepend(button);
 
 // Add a click event listener to the button
 button.addEventListener("click", function() {
