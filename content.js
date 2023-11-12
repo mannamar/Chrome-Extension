08231f0d492aaf2d6e3798a95273a34f40bcdbe8
Amardeep Mann <amar2slick@gmail.com>
2023-11-11 23:52:59 -0800

    Add button to specific container

--- content.js
+++ content.js
@@ -2,8 +2,13 @@
 var button = document.createElement("button");
 button.innerHTML = "Click me!";
 
-// Append the button to the body of the page
-document.body.prepend(button);
+// Declate container
+const skillsCont = document.getElementById("ember75");
+
+// Append the button to the container
+if (skillsCont != null) {
+  skillsCont.prepend(button);
+}
 
 // Add a click event listener to the button
 button.addEventListener("click", function() {
